cfc9cd8192cfecb2843bc1ec68dd283c6ae2c559
tempelfeldsg <tempelfelds.gustavs@gmail.com>
2026-02-11 19:40:25 +0200

    More polish

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 'use client';
 
+import Image from "next/image";
+import Link from "next/link"
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
-import { IoLogoGithub, IoLogoTwitter } from "react-icons/io";
+import { IoLogoGithub, IoLogoTwitter, IoIosMail } from "react-icons/io";
 
 export default function Home() 
 {
@@ -29,37 +31,39 @@ return (
 	<div ref={containerRef} className="relative h-[200vh] ">
 		<div className="sticky top-0 h-screen w-full overflow-hidden">
 		<motion.div className="relative w-full h-full" style={{ scale }}>
-          		<img
+          		<Image
             			src="/chessboard_side.png"
             			alt="Chess rendered"
-            			className="absolute inset-0 h-full w-full object-cover object-center"
-            			draggable={false}
+            			className="absolute inset-0 h-full w-full object-cover object-center pointer-events-none"
+				fill
+				priority
           		/>
 			<motion.div className="w-[40%] h-[30%] flex justify-start items-center flex-col top-4/10 left-1/5 absolute z-50" style={{ opacity: card_opacity, display: card_display }}>
-			<div className="text-white font-[Title] text-3xl text-center">Gustavs Tempelfelds</div>
-			<div className="text-white font-[Main] text-sm text-center">Technical Artist & Pipeline Developer</div>
-			<div className="text-white font-[Main] text-xs text-center px-24 pt-4 ">I build performant tools and systems around Blender, focusing on rendering workflows, automation, and reproducible pipelines. My work bridges art and engineering — designing infrastructure that helps artists create faster and more reliably.</div>
-			<div className="flex justify-center font-[Main] text-sm gap-8 text-white items-center pt-6">
-				<div className="h-8 w-12 flex items-center justify-center text-center">Blog</div>
-				<div className="h-8 w-12 flex items-center justify-center text-center">Portfolio</div>
-				<div className="h-8 w-12 flex items-center justify-center text-center" onClick={scrollToBottom}>Contact</div>
+			<div className="text-white font-[Title] text-4xl text-center">Gustavs Tempelfelds</div>
+			<div className="text-white font-[Main] text-xs text-center ">Technical Artist & Pipeline Developer</div>
+			<div className="text-white font-[Main] text-xs text-center px-20 pt-12 leading-relaxed ">I build performant tools and systems around Blender, focusing on rendering workflows, automation, and reproducible pipelines. My work bridges art and engineering — designing infrastructure that helps artists create faster and more reliably.</div>
+			<div className="flex justify-center font-[Main] text-sm gap-8 text-white items-center pt-8">
+				<Link href="/blog" className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70 ">Blog</Link>
+				<Link href="/portfolio" className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70">Portfolio</Link>
+				<div className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70" onClick={scrollToBottom}>Contact</div>
 			</div>
 			</motion.div>
           		<motion.div className="absolute flex items-center justify-center inset-0" style={{ clipPath }}>
-            			<img
+            			<Image
               				src="/chessboard_side_wireframe.png"
               				alt="chess wireframe"
-              				className="h-full w-full object-cover object-center"
-              				draggable={false}
+              				className="h-full w-full object-cover object-center pointer-events-none"
 					style={{ opacity: chesspiece_opacity }}
+					fill
+					priority
             			/>
 				<motion.div className="absolute top-2/6 left-1/5 flex flex-col justify-start items-center text-8xl text-center w-[40%] h-[40%] " style={{ opacity: backcard_opacity }}>
-				<div className="text-white font-[Title] text-7xl pt-16 pb-2 px-4 border-b border-zinc-600">Contact Me</div>
+				<div className="text-white font-[Title] text-7xl pt-16 pb-2 px-4 ">Contact Me</div>
 				<div className="flex items-center justify-evenly gap-8 text-zinc-300 pt-12">
-					<IoLogoGithub className="size-14" />
-					<IoLogoTwitter className="size-14" />
+					<a href="https://github.com/tempelfeldsg"><IoLogoGithub className="size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
+					<a href="#"><IoLogoTwitter className="size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
+					<a href="mailto:tempelfelds.gustavs@gmail.com"><IoIosMail className="size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
 				</div>
-				<div className="font-[Main] text-3xl pt-12 text-zinc-300">tempelfelds.gustavs@gmail.com</div>
 				
 				</motion.div>
           		</motion.div>
