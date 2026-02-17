dd07e5502ee6de06fbc985c1cc41d2cb8c7c9ef8
tempelfeldsg <tempelfelds.gustavs@gmail.com>
2026-02-17 13:45:16 +0200

    fix: main page has mobile compatibility now

--- app/page.js
+++ app/page.js
@@ -35,16 +35,16 @@ return (
           		<Image
             			src="/chessboard_side.png"
             			alt="Chess rendered"
-            			className="absolute inset-0 h-full w-full object-cover object-center pointer-events-none"
+            			className="hidden md:inline absolute inset-0 h-full w-full object-cover object-center pointer-events-none"
 				fill
 				priority
 				unoptimized
           		/>
-			<motion.div className="w-[40%] h-[30%] flex justify-start items-center flex-col top-4/10 left-1/5 absolute z-50" style={{ opacity: card_opacity, display: card_display }}>
-			<div className="text-white font-[Title] text-4xl text-center">Gustavs Tempelfelds</div>
-			<div className="text-zinc-300 font-[Main] text-xs text-center ">Technical Artist & Pipeline Developer</div>
-			<div className="text-zinc-300 font-[Main] text-xs text-center px-20 pt-12 leading-relaxed ">Work in progress</div>
-			<div className="flex justify-center font-[Main] text-sm gap-8 text-zinc-200 items-center pt-8">
+			<motion.div className="md:w-[40%] md:h-[30%] h-full w-full flex justify-start items-center flex-col top-3/10 md:top-4/10 md:left-1/5 absolute z-50" style={{ opacity: card_opacity, display: card_display }}>
+			<div className="text-white font-[Title] text-md md:text-4xl text-center">Gustavs Tempelfelds</div>
+			<div className="text-zinc-300 font-[Main] text-xs pt-1 md:pt-0 text-center ">Technical Artist & Pipeline Developer</div>
+			<div className="text-zinc-300 font-[Main] md:text-xs text-sm text-center pt-14 md:px-20 md:pt-12 leading-relaxed ">Work in progress</div>
+			<div className="flex justify-center font-[Main] text-xs md:text-sm gap-8 text-zinc-200 items-center pt-16 md:pt-8">
 				<Link href="/blog" className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70 ">Blog</Link>
 				<Link href="/portfolio" className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70">Portfolio</Link>
 				<div className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70" onClick={scrollToBottom}>Contact</div>
@@ -54,18 +54,18 @@ return (
             			<Image
               				src="/chessboard_side_wireframe.png"
               				alt="chess wireframe"
-              				className="h-full w-full object-cover object-center pointer-events-none"
+              				className="md:inline hidden h-full w-full object-cover object-center pointer-events-none"
 					style={{ opacity: chesspiece_opacity }}
 					fill
 					priority
 					unoptimized
             			/>
-				<motion.div className="absolute top-2/6 left-1/5 flex flex-col justify-start items-center text-8xl text-center w-[40%] h-[40%] " style={{ opacity: backcard_opacity }}>
-				<div className="text-white font-[Title] text-7xl pt-16 pb-2 px-4 ">Contact Me</div>
-				<div className="flex items-center justify-evenly gap-8 text-zinc-300 pt-12">
-					<a href="https://x.com/tempelfelds" target="_blank" rel="noopener noreferrer" ><FaSquareXTwitter className="size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
-					<a href="https://github.com/tempelfeldsg" target="_blank" rel="noopener noreferrer" ><IoLogoGithub className="size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
-					<a href="mailto:gustavs@tempelfelds.com"><IoIosMail className="size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
+				<motion.div className="absolute md:top-2/6 md:left-1/5 flex flex-col justify-start items-center text-center w-full h-full md:w-[40%] md:h-[40%] " style={{ opacity: backcard_opacity }}>
+				<div className="text-white font-[Title] text-4xl md:text-7xl pt-60 md:pt-16 pb-2 w-screen md:px-4 ">Contact Me</div>
+				<div className="flex items-center justify-evenly gap-18 md:gap-8 text-zinc-300 pt-20 md:pt-12">
+					<a href="https://x.com/tempelfelds" target="_blank" rel="noopener noreferrer" ><FaSquareXTwitter className="size-16 md:size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
+					<a href="https://github.com/tempelfeldsg" target="_blank" rel="noopener noreferrer" ><IoLogoGithub className="size-16 md:size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
+					<a href="mailto:gustavs@tempelfelds.com"><IoIosMail className="size-16 md:size-14 hover:text-zinc-500 duration-70 cursor-pointer" /></a>
 				</div>
 				
 				</motion.div>
