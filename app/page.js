28cf11affcf3d0943f396e460cf175b9b01a20b2
tempelfeldsg <tempelfelds.gustavs@gmail.com>
2026-02-19 23:47:23 +0200

    fix: mobile bio size

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ return (
 			<motion.div className="md:w-[40%] md:h-[30%] h-full w-full flex justify-start items-center flex-col top-3/10 md:top-4/10 md:left-1/5 absolute z-50" style={{ opacity: card_opacity, display: card_display }}>
 			<div className="text-white font-[Title] text-md md:text-4xl text-center">Gustavs Tempelfelds</div>
 			<div className="text-zinc-300 font-[Main] text-xs pt-1 md:pt-0 text-center ">Blender Tools & Pipeline Developer</div>
-			<div className="text-zinc-300 font-[Main] md:text-xs text-sm text-center pt-14 md:px-20 md:pt-12 leading-relaxed ">Technical artist and developer building Blender tools, add-ons, and production pipelines. I design systems that streamline workflows and support reliable, scalable production.</div>
+			<div className="text-zinc-300 font-[Main] text-xs text-center pt-14 md:px-20 md:pt-12 leading-relaxed ">Technical artist and developer building Blender tools, add-ons, and production pipelines. I design systems that streamline workflows and support reliable, scalable production.</div>
 			<div className="flex justify-center font-[Main] text-xs md:text-sm gap-5 text-zinc-200 items-center pt-16 md:pt-8">
 				<Link href="/blog" className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70 ">Blog</Link>
 				<Link href="/portfolio" className="h-8 w-12 flex items-center justify-center text-center hover:text-zinc-500 cursor-pointer duration-70">Portfolio</Link>
